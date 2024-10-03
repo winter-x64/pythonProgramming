a754b0e03fd26a24e333f1f146cdd1292ec88300
winter_x64 <itswinterx64@gmail.com>
2024-10-03 08:57:23 +0530

    Add Day 0 Projects

---
+++ week-0-basics/Projects/project_1.py
@@ -0,0 +1,11 @@
+# Project Story Teller
+
+# This is an example for the project
+# user have to get name or other info from the user and make story out of it
+user_name = input("Enter your name: ")
+story = f"""
+In a forgotten forest, {user_name} stumbled upon an ancient map leading to Eldoria’s lost kingdom. Armed with only courage, he ventured toward the cursed mountain, where the dragon’s whispers beckoned. Time was running out. {user_name}’s fate, and that of Eldoria, hung on his next fateful choice.
+"""
+print(story)
+
+# Try building a one for yourself
